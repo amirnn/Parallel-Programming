e49ba8af644e15d2bfbd246eb8d662fc5cee3bb0
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-05-05 22:35:06 +0200

    ToDo: Done.

--- Demo/helloworldParallel.cc
+++ Demo/helloworldParallel.cc
@@ -5,12 +5,17 @@
 
 long unsigned num_steps{100'000};
 
-unsigned numThreads = omp_get_num_threads();
-unsigned itrrCountForEachThread = floor(num_steps / numThreads);
-unsigned remainderSteps = num_steps % numThreads;
+
 
 int main(int argc, char const *argv[])
-{
+{   
+    // Serial part for setup
+    // unsigned numThreads = omp_get_num_threads(); 
+    unsigned numThreads = omp_get_max_threads();
+    omp_set_num_threads(numThreads);
+    unsigned itrrCountForEachThread = floor(num_steps / numThreads);
+    unsigned remainderSteps = num_steps % numThreads;
+
     // parallel code
     long double pi, sum{0.0};
     double step{1.0 / num_steps};
@@ -26,13 +31,18 @@ int main(int argc, char const *argv[])
         }
     }
 
+    // Thread team joins here -- serial execution again starts from here.
+    
     // ToDo: Do the remaining summation of the elements since I used mod operataor above to
     // determine the number of threads.
-    
+    long double x;
+    for(unsigned i = 0; i < remainderSteps; i++){
+        x = 1 - ((i + .5) * step);
+            sum += 4.0 / (1.0 + pow(x, 2));
+    }
 
     // To reduce Error we do this operation in the end.
     pi = step * sum;
-
     std::cout << std::setprecision(20);
     std::cout << "The sum is: " << pi << std::endl;
     std::cout << "Number of threads used: " << numThreads << std::endl;
