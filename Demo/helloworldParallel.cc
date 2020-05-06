dcdf8a32d717a83b04e86be832bc391e38535d13
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-05-06 17:27:19 +0200

    My Version of HelloWorld with mistakes! :D

--- Demo/helloworldParallel.cc
+++ Demo/helloworldParallel.cc
@@ -5,26 +5,27 @@
 
 long unsigned num_steps{100'000};
 
-
-
 int main(int argc, char const *argv[])
 {   
     // Serial part for setup
     // unsigned numThreads = omp_get_num_threads(); 
     unsigned numThreads = omp_get_max_threads();
+    std::cout << "Number of threads used: " << numThreads << std::endl;
     omp_set_num_threads(numThreads);
     unsigned itrrCountForEachThread = floor(num_steps / numThreads);
     unsigned remainderSteps = num_steps % numThreads;
-
-    // parallel code
+    std::cout << "itrrCountForEachThread: " << itrrCountForEachThread << std::endl;
+    // parallel code starts here.
     long double pi, sum{0.0};
     double step{1.0 / num_steps};
     #pragma omp parallel num_threads(numThreads)
     {   
         long double x; // update x according to the index of the thread.
-        int thisThreadNum = omp_get_thread_num();
+        unsigned thisThreadNum = omp_get_thread_num();
+        std::cout << "thisThreadNum: " << thisThreadNum << std::endl;
         unsigned domainShift = thisThreadNum * itrrCountForEachThread;
-        for (unsigned i = 0; i < itrrCountForEachThread; i++)
+        std::cout << "domainShift: " << domainShift << std::endl;
+        for (unsigned i = 0; i < itrrCountForEachThread; ++i)
         {   
             x = (i + .5 + domainShift) * step;
             sum += 4.0 / (1.0 + pow(x, 2));
@@ -32,7 +33,7 @@ int main(int argc, char const *argv[])
     }
 
     // Thread team joins here -- serial execution again starts from here.
-    
+
     // ToDo: Do the remaining summation of the elements since I used mod operataor above to
     // determine the number of threads.
     long double x;
@@ -45,6 +46,5 @@ int main(int argc, char const *argv[])
     pi = step * sum;
     std::cout << std::setprecision(20);
     std::cout << "The sum is: " << pi << std::endl;
-    std::cout << "Number of threads used: " << numThreads << std::endl;
     return 0;
 }
